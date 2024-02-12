f47cc9ac877c6b81217f9c3857b3f55bb3d9e056
Dorian Rousse <biragolouis@gmail.com>
2024-02-12 16:16:31 +0100

    Modification de la version du sdk pour 3.2

--- tp2/lib/main.dart
+++ tp2/lib/main.dart
@@ -1,24 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MaterialApp(
-  home: Home(),
-));
+class Todo {
+  final String title;
+  final String description;
 
-class Home extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "TP2",
-            style: TextStyle(
-              fontFamily: "PlayfairDisplay",
-              ),
-            ),
-          centerTitle:true,
-          backgroundColor: Colors.red[600],
-        ), 
-        //body: 
+  const Todo(this.title, this.description);
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "TP2",
+      home: TodosScreen(
+        todos: List.generate(
+          20,
+          (i) => Todo(
+            'Todo $i',
+            'A description of what needs to be done for Todo $i',
+          ),
+        ),
+      ),
+    ),
   );
+}
+
+class TodosScreen extends StatelessWidget {
+  const TodosScreen({Key? key, required this.todos}) : super(key: key);
+
+  final List<Todo> todos;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'TP2',
+          style: TextStyle(
+            fontFamily: "PlayfairDisplay",
+            fontSize: 30,
+          ),
+        ),
+        centerTitle: true, 
+        backgroundColor: Colors.red[900],
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(todo: todos[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  final Todo todo;
+
+  const DetailScreen({Key? key, required this.todo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Text(todo.description),
+      ),
+    );
   }
 }
